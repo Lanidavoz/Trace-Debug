0762f746f68d5735844b3ba4b65a3f25f5a7fb87
marcrowland <marcrowland@outlook.com>
2021-02-21 20:14:58 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace dotNetTracing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This message is readable by the end user.");
+            Trace.WriteLine("This is a trace message when tracing the app.");
+            Debug.WriteLine("This is a debug message just for developers.");
+        }
+    }
+}
